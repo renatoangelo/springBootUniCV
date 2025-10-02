d608aef8f3d675b0551e1d344bafe73b0ccd697e
Nicole Novoli <araujonovoli@gmail.com>
2025-10-01 21:24:27 -0300

    Entidade Paciente

---
+++ src/main/java/com/example/demo/Entities/Paciente.java
@@ -0,0 +1,37 @@
+package com.example.demo.Entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Entity
+@Table(name = "pacientes")
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+
+public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column(nullable = false)
+    private String telefone;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}
